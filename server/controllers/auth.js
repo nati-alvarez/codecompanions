055c22461ee66dd7d7d934d3ec5c384321a375aa
Natividad Alvarez <tivialvarezs@gmail.com>
2018-02-28 17:20:00 -0500

    Add signup and verify route. Configure email server and send verification email on signup.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,10 +3,24 @@ const uuid = require("uuid/v1");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+//email server for sending verification email
+const Email = require('email-templates');
+
+
+const email = new Email({
+    message: {
+      from: 'codecompanions@zoho.com'
+    },
+    transport: {
+      jsonTransport: true
+    }
+});
+
 exports.login = (req, res) => {
     User.findOne({username: req.body.username}).then(user => {
         if(!user) return res.status(401).json({success: false, message: "User not found."});
         if(!bcrypt.compareSync(req.body.password, user.password)) return res.status(401).json({success: false, message: "Incorrect username or password."});
+        if(user.verified !== 1) return res.status(401).json({success: false, message: "Please verify your account to log in."});
         var token = jwt.sign({user}, process.env.JWT_SECRET);
         res.status(200).json({success: true, user, token});
     }).catch(err => {
@@ -16,17 +30,39 @@ exports.login = (req, res) => {
 
 exports.signup = (req, res) => {
     var newUser = new User({
-        username: req.body.username,
         name: req.body.name,
+        username: req.body.username,
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 10),
         verificationCode: uuid(),
     });
-
+    
     newUser.save().then(user =>{ 
         res.status(201).json({success: true, user, message: "Account created. Check your email to verify your account."});
+        //send verification email
+        email.send({
+            template: 'verify',
+            message: {
+                to: user.email
+            },
+            locals: {
+                name: user.name.split(" ")[0],
+                verificationCode: user.verificationCode
+            }
+        })
+        .then(console.log)
+        .catch(console.error);
     }).catch(err => {
         res.status(500).json({success: false, err, message: "An error occured while signing up."});
+    });
+}
+
+exports.verify = (req, res) => {
+    User.findOneAndUpdate({verificationCode: req.body.verificationCode}, {set: {verified: 1}}, {new: true}).then(user => {
+        if(!user) return res.status(401).json({success: false, message: "Invalid token."});
+        res.status(201).json({success: true, message: "Your account was verified. You may now log in."});
+    }).catch(err => {
+        res.status(500).json({success: false, err, message: "An error occurred. Please try again."});
     });
 }
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,5 +4,6 @@ const passport = require("passport");
 
 router.post("/login", controller.login);
 router.post("/signup", controller.signup);
+router.post("/verify", controller.verify);
 
 module.exports = router;
