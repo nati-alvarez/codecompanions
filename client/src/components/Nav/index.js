7d39e815c3a7c1daeb9aae084415707d6961e170
Natividad Alvarez <tivialvarezs@gmail.com>
2018-03-01 22:45:53 -0500

    Create nav component.

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+    render(){
+        return (
+            <nav className="top-bar">
+                <div className="top-bar-left menu">
+                    <a className="logo" href="#">_CODE COMPANIONS</a>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
