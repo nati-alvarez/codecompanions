4c1100ead27ed4f342859f1e819bb7742655ad93
Natividad Alvarez <tivialvarezs@gmail.com>
2018-03-04 04:26:37 -0500

    Create action for sending reset password email

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -9,15 +9,18 @@ export default function reducer(state={
     switch(action.type){
         case "LOGIN_START":
         case "SIGNUP_START":
+        case "RECOVER_EMAIL_START":
             return {...state, form: {...state.form, loading: true, errorMessage: null, successMessage: null}}
         case "LOGIN_SUCCESS":
             document.getElementById("submit-button").disabled = false;
             return {...state, user: action.payload.user, form: {...state.form, loading: false}}
         case "SIGNUP_SUCCESS":
+        case "RECOVER_EMAIL_SUCCESS":
             document.getElementById("submit-button").disabled = false;
             return {...state, form: {...state.form, loading: false, errorMessage: null, successMessage: action.payload}}        
         case "LOGIN_ERR":
         case "SIGNUP_ERR":
+        case "RECOVER_EMAIL_ERR":
         case "VALIDATION_ERR":
             document.getElementById("submit-button").disabled = false;
             return {...state, form: {...state.form, loading: false, errorMessage: action.payload}}
