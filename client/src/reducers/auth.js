e577980cb783b0523c741edbb5217a14f2ac8fe5
Natividad Alvarez <tivialvarezs@gmail.com>
2018-03-03 23:15:54 -0500

    Update reducer to listen for login/signup form actions.

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,7 +1,28 @@
 export default function reducer(state={
     user: JSON.parse(localStorage.getItem("user")) || null,
+    form: {
+        loading: false,
+        errorMessage: null,
+        successMessage: null
+    }
 }, action){
     switch(action.type){
+        case "LOGIN_START":
+        case "SIGNUP_START":
+            return {...state, form: {...state.form, loading: true, errorMessage: null, successMessage: null}}
+        case "LOGIN_SUCCESS":
+            document.getElementById("submit-button").disabled = false;
+            return {...state, user: action.payload.user, form: {...state.form, loading: false}}
+        case "SIGNUP_SUCCESS":
+            document.getElementById("submit-button").disabled = false;
+            return {...state, form: {...state.form, loading: false, errorMessage: null, successMessage: action.payload}}        
+        case "LOGIN_ERR":
+        case "SIGNUP_ERR":
+        case "VALIDATION_ERR":
+            document.getElementById("submit-button").disabled = false;
+            return {...state, form: {...state.form, loading: false, errorMessage: action.payload}}
+        case "SWAP_FORM":
+            return {...state, form: {...state.form, loading: false, errorMessage: null, successMessage: null}}
         default:
             return state;
     }
