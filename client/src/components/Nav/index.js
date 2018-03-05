d67f6d09e9c6f986b3b3becd46d5cb0084853593
Natividad Alvarez <tivialvarezs@gmail.com>
2018-03-05 16:56:31 -0500

    Update nav to show username if logged in

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 class Nav extends Component {
     render(){
         return (
             <nav className="top-bar">
-                <div className="top-bar-left menu">
-                    <a className="logo" href="#">_CODE COMPANIONS</a>
-                </div>
+                <ul className="top-bar-left menu">
+                    <Link to="/" className="logo">_CODE COMPANIONS</Link>
+                </ul>
+                <ul className="top-bar-right menu">
+                    {this.props.user &&
+                        <li>{this.props.user.username}</li>
+                    }
+                </ul>
             </nav>
         );
     }
 }
 
-export default Nav;
+const mapStateToProps = (state) => {
+    return {
+        user: state.auth.user
+    }
+}
+
+export default connect(mapStateToProps)(Nav);
