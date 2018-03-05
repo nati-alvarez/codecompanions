241a60eebedba4c0db658bb10e2e56f8b3e2112e
Natividad Alvarez <tivialvarezs@gmail.com>
2018-03-05 17:04:46 -0500

    Rename send password recovery email route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,7 @@ router.post("/login", controller.login);
 router.post("/signup", controller.signup);
 router.post("/verify", controller.verify);
 router.post("/resend-email", controller.resendVerificationEmail);
-router.post("/send-recover-password", controller.sendPasswordRecoveryEmail);
+router.post("/send-recover-email", controller.sendPasswordRecoveryEmail);
 router.post("/reset-password", controller.resetPassword);
 
 module.exports = router;
