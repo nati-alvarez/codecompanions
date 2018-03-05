7b8ac19ff4becdc1d8c4aa1118a3d0e0774c19c9
Natividad Alvarez <tivialvarezs@gmail.com>
2018-03-05 16:59:48 -0500

    Add user login check. Redirect to / if user is not logged in. Render nav on dashboard

--- client/src/components/dashboard/index.js
+++ client/src/components/dashboard/index.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+//COMPONENTS
+import Nav from '../Nav';
+
+//STYLES
+import '../../styles/pages/dash.sass';
 
 class Dash extends Component {
+    componentDidMount(){
+        if(!this.props.user) this.props.history.push("/");
+    }
+    componentDidUpdate(){
+        if(!this.props.user) this.props.history.push("/");
+    }
     render(){
         return (
-            <div>This is the dashboard</div>
+            <div className="dashboard">
+                <Nav/>
+                <main>
+                    This is the dashboard
+                </main>
+            </div>
         );
     }
 }
 
-export default Dash;
+const mapStateToProps = (state) => {
+    return {
+        user: state.auth.user
+    }
+}
+
+export default connect(mapStateToProps)(Dash);
