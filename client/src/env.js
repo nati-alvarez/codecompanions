fa42f63af1faf6ca821e4fd14aa3800740eaa1ac
Natividad Alvarez <tivialvarez@gmail.com>
2020-02-16 13:33:44 -0500

    use production api url

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 //PRODUCTION
-// exports.API_URL = 'https://codecompanions.herokuapp.com/api';
+exports.API_URL = 'https://codecompanions.herokuapp.com/api';
 
 //DEVELOPMENT
-exports.API_URL = "http://localhost:8000/api";
+// exports.API_URL = "http://localhost:8000/api";
